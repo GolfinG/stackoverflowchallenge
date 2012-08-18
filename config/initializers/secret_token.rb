43ad75ed43e09d2b449a2a054a8d84e782f51486
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2012-08-18 15:54:16 -0700

    initial season 2 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stackoverflowchallenge::Application.config.secret_token = 'f5e8c2c8cc8abfa8f33211375f46b3af3bde6281eb4b3c692ef3ab0bbb9f819341364ce853e040e0f33fdca54c4be0d24b931024dca7ddece06143caf0751a70'
